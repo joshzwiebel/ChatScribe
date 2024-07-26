7c4020269f085be8aa6109e6badd3fb497571444
joshzwiebel <joshzwiebel@gmail.com>
2024-07-25 23:20:39 -0400

    new

---
+++ plan.md
@@ -0,0 +1,12 @@
+things to do
+- find out if streamlit can do mobile
+- read the qa link on openai docs
+- make a command line app that saves to a file
+-  make an app app with persistent memory
+
+
+CLi structure commands
+- add note
+- ask a question
+- read out my data
+- delete / edit my data
--- src/main.py
+++ src/main.py
@@ -5,20 +5,27 @@ import os
 # Set your OpenAI API key
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+
+
+
+
+@cli.command(help="saves input to a file")
 @click.option('--filename', prompt='Please enter the filename', help='The name of the file to save the input to.')
 @click.option('--content', prompt='Please enter the content', help='The content to write to the file.')
 def save_input_to_file(filename, content):
     """This is a command line application that saves user input to a file."""
     if os.path.exists(filename):
         click.echo(f'File {filename} already exists.')
-        if click.confirm('Do you want to read it?'):
-            with open(filename, 'r') as file:
-                click.echo(file.read())
+        with open(filename, 'a') as file:
+            file.write(content)
     else:
         with open(filename, 'w') as file:
             file.write(content)
         click.echo(f'Successfully wrote to {filename}')
 
 if __name__ == '__main__':
-    save_input_to_file()
+    cli()
